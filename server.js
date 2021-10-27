1eb55cd9544149e02f57e21ccb1b724f1bd6b82c
Matt DiPaolo <matt@dldipaolo.com>
2021-10-27 15:02:47 -0400

    added the server.js and npm files

---
+++ server.js
@@ -0,0 +1,44 @@
+const ccxt = require('ccxt');
+
+var kraken = new ccxt.kraken({id: "kraken"});
+var coinbase  = new ccxt.coinbase({id: "coinbase" });
+var exchanges = [kraken, coinbase];
+
+const markets = ["BTC/USD", "ETH/USD"];
+
+
+
+
+//main
+(async function() {
+    console.log("Loading exchanges' markets")
+    await loadExchangeMarkets()
+    console.log("Loaded exchanges' markets\n")
+
+    
+
+
+
+})()
+
+
+//loads the markets into the phemex and bybit objects
+async function loadExchangeMarkets()
+{
+    for (var exchange of exchanges){
+        try {
+            await exchange.loadMarkets()
+        } catch (err) {
+            console.error(err)
+            console.log("Error in loadExchangeMarkets")
+            console.log(`Error loading ${exchange.id} markets`)
+            terminateProgram()
+        }
+    }
+}
+//informs the user the program will be terminated then ends the program
+function terminateProgram(){
+    console.log("\n\nProgram terminated\n\n")
+    process.exit()
+}
+
