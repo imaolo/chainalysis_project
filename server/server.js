41ded3f097089996d4b4111652cbd009ad90002b
Matt DiPaolo <matt@dldipaolo.com>
2021-10-28 14:16:02 -0400

    refactored server.js and added the updated functionality for the client

--- server/server.js
+++ server/server.js
@@ -1,20 +1,47 @@
 const ccxt = require('ccxt');
 const express = require('express')
 
+//create each exchange object and add it to the exchanges array
 var kraken = new ccxt.kraken({id: "kraken"});
-var coinbase  = new ccxt.coinbase({id: "coinbase" });
-var exchanges = [kraken, coinbase];
-
+var bybit  = new ccxt.bybit({id: "bybit" });
+var exchanges = [kraken, bybit];
+//specify which markets we are monitoring
 const markets = ["BTC/USD", "ETH/USD"];
-//ccxt setup function
+//tickers object to hold the data. Populate it with the required key value pairs
+var tickers = {}
+exchanges.forEach(exchange => {
+    tickers[exchange.id] = {}
+    markets.forEach(market => {
+        tickers[exchange.id][market] = {}
+    })
+});
+
+//main
 (async function() {
-    console.log("Loading exchanges' markets")
     await loadExchangeMarkets()
-    console.log("Loaded exchanges' markets\n")
+
+    //preliminarly load the data into the ticker object
+    await retrieveTickers()
+
+    //every interval milliseconds, fetch the tickers
+    var interval = 5000
+    setInterval(retrieveTickers,interval)
+    
+    //web server setup (AKA the listener)
+    const app = express();
+    const port = 3000;
+    app.use("/",express.static('../client'))
+    app.get('/tickers',(req,res) => {
+        res.json(tickers)
+    })
+    app.listen(port, () => {
+        console.log(`listening at http://localhost:${port}`)
+    })
 })()
 
 
-//loads the markets into the phemex and bybit objects
+
+//loads loads the exchange objects with the markets that they support
 async function loadExchangeMarkets()
 {
     for (var exchange of exchanges){
@@ -28,19 +55,32 @@ async function loadExchangeMarkets()
         }
     }
 }
-//informs the user the program will be terminated then ends the program
+//write the market data to the tickers object
+async function retrieveTickers(){
+    //make our request asynchronoulsy
+    var promises = {}
+    for (var exchange of exchanges){
+        try{
+            promises[exchange.id] = exchange.fetchTickers(markets)
+        } catch(err){
+            console.error(err)
+            console.log(`Error retrieving ${exchange} tickers`);
+        }
+    }
+    //the promises object holds the promises of each exchange. i.e. promise = {kraken: promise, bybit: promise}
+    //we can now listen for the requests to be fullfilled
+    for (let [exchange,promise] of Object.entries(promises)){
+        let data = await promise;
+        for (let [market,ticker] of Object.entries(data)){
+            tickers[exchange][market] = {
+                bid: `${ticker.bid}`,
+                ask: `${ticker.ask}`
+            }
+        }
+    }
+}
+//informs the user the program will be terminated and then ends the program
 function terminateProgram(){
     console.log("\n\nProgram terminated\n\n")
     process.exit()
 }
-
-const app = express();
-const port = 3000;
-
-app.use("/",express.static('../client'))
-
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
-})
-
-
